4168042fa494403e9598cb96c46e53bc65026da1
Your Name <you@example.com>
2018-08-12 20:21:36 +0530

    json schema

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace jschema
+{
+    class Program
+    {
+        public async void test(string path){
+            string schemaData = System.IO.File.ReadAllText(path);
+            var schema = await JsonSchema4.FromJsonAsync(schemaData);
+            var generator = new CSharpGenerator(schema);
+            var file = generator.GenerateFile();
+             System.IO.File.WriteAllText("D:/Result.cs", file);
+        }
+        static void Main(string[] args)
+        {
+            Person per = new Person{ MiddleName = "pradeep"};
+
+            Program pgm = new Program();
+            pgm.test(args[0]);
+        }
+    }
+}
